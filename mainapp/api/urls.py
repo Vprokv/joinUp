9cb0e0b86bd283a3eb2efac573396b58e3106971
veronikageidt <prokopchuk_veron@mail.ru>
2021-11-27 16:28:46 +0500

    urls pattern

--- mainapp/api/urls.py
+++ mainapp/api/urls.py
@@ -2,6 +2,25 @@ from django.urls import path
 
 from .views import ProgramTestAPIView
 
-urlpatterns = [
-    path('test-api/', ProgramTestAPIView.as_view(), name='program')
-]
+urlpatterns = {
+    path('program/', ProgramTestAPIView.as_view(), name='program'),
+    path('adaptationprogram/', AdaptationProgramTestAPIView.as_view(), name='adaptation program'),
+    path('adaptationlevel/', AdaptationLevelTestAPIView.as_view(), name='adaptation level'),
+    path('adaptationstage/', AdaptationStageTestAPIView.as_view(), name='adaptation stage'),
+    path('adaptationblock/', AdaptationBlockTestAPIView.as_view(), name='adaptation block'),
+    path('adaptationbgoal/', AdaptationGoalTestAPIView.as_view(), name='adaptation goal'),
+    path('adaptationdocument/', AdaptationDocumentTestAPIView.as_view(), name='adaptation document'),
+    path('adaptationcontact/', AdaptationContactTestAPIView.as_view(), name='adaptation contact'),
+    path('lnklevelprogram/', LnkLevelProgramTestAPIView.as_view(), name='lnk level program'),
+    path('lnkstagelevel/', LnkStageLevelTestAPIView.as_view(), name='lnk stage level'),
+    path('lnkgoalprogram/', LnkGoalProgramTestAPIView.as_view(), name='lnk goal program'),
+    path('lnkdocumentprogram/', LnkDocumentProgramTestAPIView.as_view(), name='lnk document program'),
+    path('lnkpcontactrogram/', LnkContactProgramTestAPIView.as_view(), name='lnk contact program'),
+    path('customer/', CustomerTestAPIView.as_view(), name='customer'),
+    path('licensepack/', LicensePackTestAPIView.as_view(), name='license Pack'),
+    path('userservice/user/', UserServiceUserTestAPIView.as_view(), name='user service(user)'),
+    path('userservice/sms/', UserServiceSMSTestAPIView.as_view(), name='user service(sms)'),
+    path('userservice/token/', UserServiceTokenTestAPIView.as_view(), name='user service(token)'),
+    path('userservice/candidate/', UserServiceCandidateTestAPIView.as_view(), name='candidate'),
+    path('employee/', EmployeTestAPIView.as_view(), name='candidate'),
+}
--- mainapp/api/views.py
+++ mainapp/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+# from snippets.
 
 
 class ProgramTestAPIView(APIView):
@@ -7,3 +9,12 @@ class ProgramTestAPIView(APIView):
     def get(self, request, *args, **kwargs):
         data = [{"id": 1, "name": "John"}, {"id": 2, "name": "John"}]
         return Response(data)
+
+    def post(self, request, *args, **kwargs):
+        return Response({"id": 3, "name": "axaxa"}, status=status.HTTP_201_CREATED)
+
+    # def put(self, request, *args, **kwargs):
+    #     return Response(status=status.HTTP_200_OK)
+
+    def delete(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_204_NO_CONTENT)
