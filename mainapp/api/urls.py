789bf5c77b3778a21fce7f8a621ead4d5ff20c3f
veronikageidt <prokopchuk_veron@mail.ru>
2021-11-28 11:35:22 +0500

    more urls project

--- mainapp/api/urls.py
+++ mainapp/api/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import ProgramTestAPIView
 
-urlpatterns = {
+urlpatterns = {  # http://127.0.0.1:8000/api/
     path('program/', ProgramTestAPIView.as_view(), name='program'),
+    # Сервис old
+    # IAdaptationProgramService
     path('adaptationprogram/', AdaptationProgramTestAPIView.as_view(), name='adaptation program'),
     path('adaptationlevel/', AdaptationLevelTestAPIView.as_view(), name='adaptation level'),
     path('adaptationstage/', AdaptationStageTestAPIView.as_view(), name='adaptation stage'),
@@ -16,11 +18,25 @@ urlpatterns = {
     path('lnkgoalprogram/', LnkGoalProgramTestAPIView.as_view(), name='lnk goal program'),
     path('lnkdocumentprogram/', LnkDocumentProgramTestAPIView.as_view(), name='lnk document program'),
     path('lnkpcontactrogram/', LnkContactProgramTestAPIView.as_view(), name='lnk contact program'),
+    # IСustomerService
     path('customer/', CustomerTestAPIView.as_view(), name='customer'),
     path('licensepack/', LicensePackTestAPIView.as_view(), name='license Pack'),
+    # IUserService
     path('userservice/user/', UserServiceUserTestAPIView.as_view(), name='user service(user)'),
     path('userservice/sms/', UserServiceSMSTestAPIView.as_view(), name='user service(sms)'),
     path('userservice/token/', UserServiceTokenTestAPIView.as_view(), name='user service(token)'),
     path('userservice/candidate/', UserServiceCandidateTestAPIView.as_view(), name='candidate'),
+    # IEmployeeService
     path('employee/', EmployeTestAPIView.as_view(), name='candidate'),
+
+    #     Сервисы для мобильного
+    path('iemployeeserviceauthentification/', IEmployeeServiceAuthentificationTestAPIView.as_view(),
+         name='service authentication'),
+    path('requestusercandidatetoken/', RequestUserToken.as_view(), name='request user candidate token'),
+    path('iadaptationprogram/', IAdaptationProgramTestAPIView.as_view(), name='i adaptation program'),
+    path('ilevelstages/', ILevelStagesTestAPIView.as_view(), name='i level stages'),
+    path('igoals/', IGoalsTestAPIView.as_view(), name='i goals'),
+    path('idocuments/', IDocumentsTestAPIView.as_view(), name='i documents'),
+    path('icontacts/', IContactsTestAPIView.as_view(), name='i contacts'),
+    path('iblocks/', IBlocksTestAPIView.as_view(), name='i blocks')
 }
